8f0f931bbffecf7a90c23050a15be211d85abcc3
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-03 13:37:26 +0200

    chore: try again

--- client/components/select.js
+++ client/components/select.js
@@ -2,6 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AsyncSelect from 'react-select/async';
 
+const optionStyles = props => base => {
+  return {
+    ...base,
+    color: props.isFunky ? 'pink' : 'black',
+    paddingLeft: '8px',
+    paddingRight: '8px',
+  };
+};
+
+const createSelectStyles = props => ({
+  option: optionStyles(props),
+});
+
 const options = [
   { value: 'hargow', label: 'Crystal shrimp dumplings (har gow)' },
   { value: 'gaimeibao', label: 'Coconut buns (gai mei bao)' },
@@ -13,6 +26,7 @@ const loadOptions = () => Promise.resolve(options);
 const SelectComponent = props => (
   <AsyncSelect
     id="select-input"
+    styles={createSelectStyles(props)}
     value={props.value}
     defaultValue={options[0]}
     searchable={true}
@@ -28,6 +42,12 @@ SelectComponent.propTypes = {
     value: PropTypes.string,
     label: PropTypes.string,
   }),
+  // lol
+  isFunky: PropTypes.bool.isRequired,
+};
+
+SelectComponent.defaultProps = {
+  isFunky: true,
 };
 
 export default SelectComponent;
